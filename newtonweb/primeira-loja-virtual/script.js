13398bfc9432709db997f113fac085ec224dd5e9
GersonESS <gersones@outlook.com>
2024-11-09 07:25:59 -0300

    a

---
+++ newtonweb/primeira-loja-virtual/script.js
@@ -0,0 +1,17 @@
+/*let number = 1;
+var x = 2;
+
+
+
+var sum = (x,y) => x + y;
+
+
+console.log(sum(number,x));
+*/
+/*
+var myMain = document.getElementById('main');
+myMain.style.color = 'red';
+*/
+/*myMain.style.backgroundColor = 'black';*/
+var itens = document.getElementsByClassName('container');
+itens[0].style.backgroundColor='blue';
